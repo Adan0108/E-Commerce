2e7800fb71864695b669fec7ebf5d6e353cb28dd
Quang Anh <lequanganh123@gmail.com>
2025-03-04 16:20:22 +1100

    finish create resposnc and error

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,19 +1,28 @@
 'use strict'
 const AccessService = require('../services/access.service')
+
+const { OK , CREATED} = require('../core/success.response')
 /*
 200 OK
 201 CREATED
 */
 class AccessController {
   signUp = async (req, res, next) => {
-    try {
-      console.log(`[P]::signUp::`, req.body)
-      return res.status(201).json(
-        await AccessService.signUp(req.body)
-      )
-    } catch (error) {
-      next(error)
-    }
+    // try {
+      // console.log(`[P]::signUp::`, req.body)
+      // return res.status(201).json(
+      //   await AccessService.signUp(req.body)
+      // )
+    // } catch (error) {
+    //   next(error)
+    // }
+    new CREATED({
+      message: 'RegisterOK!',
+      metadata: await AccessService.signUp(req.body),
+      options:{
+        limit: 10
+      }
+    }).send(res)
   }
 }
 module.exports = new AccessController();

794ef5bd4b15aaa84305f9e51bc9e171bd5e48bc
Quang Anh <lequanganh123@gmail.com>
2025-03-25 11:44:48 +1100

    Inventory model and service(repo)

--- src/services/product.service.xxx.js
+++ src/services/product.service.xxx.js
@@ -11,6 +11,7 @@ const { findAllDraftsForShop,
     findProduct,
   updateProductById } = require('../models/repositories/product.repo')
 const { removeUndefinedObject,updateNestedObjectParse } = require('../utils')
+const { insertInventory } = require('../models/repositories/inventory.repo')
 
 // define Factory class to create product
 class ProductFactory {
@@ -87,8 +88,17 @@ class Product{
   }
   //create new product
   async createProduct(product_id){
-    return await product.create({...this, _id:product_id})
-  }
+    const newProduct = await product.create({...this, _id:product_id})
+    if(newProduct){
+      //add product_stock in inventory collection
+      await insertInventory({
+        productId: newProduct._id,
+        shopId: this.product_shop,
+        stock: this.product_quantity
+      })
+    }
+      return newProduct
+    }
 
   //update Product
   async updateProduct( productId , bodyUpdate ){

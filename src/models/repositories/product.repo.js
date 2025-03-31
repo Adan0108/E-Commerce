ba27637924d8930c60ea8f52e3f36b3c2d3c249f
Quang Anh <lequanganh123@gmail.com>
2025-03-31 21:47:16 +1100

    cart(model,service,controller,router) delete function is incomplete and need fixed

--- src/models/repositories/product.repo.js
+++ src/models/repositories/product.repo.js
@@ -2,7 +2,7 @@
 
 const { product, electronic, clothing, furniture } = require('../product.model')
 const { Types: { ObjectId } } = require('mongoose')
-const{ getSelectData, unGetSelectData } = require('../../utils')
+const{ getSelectData, unGetSelectData, convertToObjectIdMongodb } = require('../../utils')
 const findAllDraftsForShop = async ({ query, limit, skip }) => {
   return await queryProduct({ query, limit, skip })
 }
@@ -94,6 +94,10 @@ const queryProduct = async ({ query, limit, skip }) => {
     .exec()
 }
 
+const getProductById = async (productId) =>{
+  return await product.findOne({ _id: convertToObjectIdMongodb(productId)}).lean()
+}
+
 module.exports = {
   findAllDraftsForShop,
   findAllPublishForShop,
@@ -102,5 +106,6 @@ module.exports = {
   searchProductByUser,
   findAllProducts,
   findProduct,
-  updateProductById
+  updateProductById,
+  getProductById
 }  
